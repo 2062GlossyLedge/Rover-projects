b5efa8fd951962041f2ed90019aaa7ef16db114a
Ayden Smith <aysmith17@gmail.com>
2021-07-03 21:53:53 -0600

    Add files via upload
    Motor control

---
+++ autonomous rover #1/a1_rover.py
@@ -0,0 +1,71 @@
+import RPi.GPIO as io
+from time import sleep
+
+in1 = 24
+in2 = 23
+enA = 25
+temp1=1
+
+in3 = 20
+in4 = 21
+enB = 16
+
+io.setmode(io.BCM)
+
+#First motor
+io.setup(in3,io.OUT)
+io.setup(in4,io.OUT)
+io.setup(enB,io.OUT)
+io.output(in3,io.LOW)
+io.output(in4,io.LOW)
+p2_dt = io.PWM(enB,1000)
+p2_dt.start(25)
+#Second motor
+io.setup(in1,io.OUT)
+io.setup(in2,io.OUT)
+io.setup(enA,io.OUT)
+io.output(in1,io.LOW)
+io.output(in2,io.LOW)
+p_dt= io.PWM(enA,1000)
+p_dt.start(25)
+
+while(1):
+    command = input("Enter 'r' to make the rover drive: ")
+
+    if command == 'r':
+        #forwards
+        io.output(in3, io.HIGH)
+        io.output(in4, io.LOW)
+        p2_dt.ChangeDutyCycle(100)
+        io.output(in1, io.HIGH)
+        io.output(in2, io.LOW)
+        p_dt.ChangeDutyCycle(100)
+        sleep(10)
+        #stop
+        io.output(in3, io.LOW)
+        io.output(in4, io.LOW)
+        io.output(in1, io.LOW)
+        io.output(in2, io.LOW)
+        sleep(3)
+        #backwards
+        io.output(in3, io.LOW)
+        io.output(in4, io.HIGH)
+        p2_dt.ChangeDutyCycle(100)
+        io.output(in1, io.LOW)
+        io.output(in2, io.HIGH)
+        p2_dt.ChangeDutyCycle(100)
+        sleep(10)
+        #stop
+        io.output(in3, io.LOW)
+        io.output(in4, io.LOW)
+        io.output(in1, io.LOW)
+        io.output(in2, io.LOW)
+
+    elif command == 's':
+        io.cleanup()
+        break
+
+    else:
+        print("You entered an invalid command. Try again")
+
+
